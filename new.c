dd901882d1bf95cce213a38141ea7f6834f84951
Sreenandhuu <sreenandhuvenu367@gmail.com>
2026-06-14 20:31:03 +0530

    4th

---
+++ new.c
@@ -0,0 +1,54 @@
+// #include<stdio.h>
+// int main(){
+//     int i,j,k,p=1,m=1;
+//     for(i=1;i<=11;i++){
+//         k=i%3==0?m:p;
+//     for(j=1;j<=k;j++){
+//         if(i%3==0){
+//             if(j%2==0)
+//             printf("$*$");
+//             else printf("*$*");
+//         }else printf("*");
+
+//     }printf("\n");
+//     if(i%3==0)
+//     {
+//         m++;
+//         p++;
+//     }
+//     }printf("*$*$*$*$*$");
+// }
+//concatenat
+
+// #include<stdio.h>
+// int con(int a[],int na,int b[],int nb,int c[]){
+//     int k=0;
+// for(int i=0;i<na;i++){
+//     c[k++]=a[i];
+// }for(int i=0;i<nb;i++){
+//     c[k++]=b[i];
+// }
+// }
+// int  main(){
+//     int a[]={1,2,3},na=3,nb=3,b[]={4,5,6},c[6];
+//     con(a,na,b,nb,c);
+//     for(int i=0;i<6;i++)
+// printf("%d ",c[i]);
+// }
+// Create a script that separates even and odd numbers into separate arrays from a given 
+// array of integers. 
+// #include<stdio.h>
+// int main(){
+//     int a[]={1,2,3,4,5},n=5,i,j;
+//     printf("even");
+//     for(i=0;i<n;i++){
+//         if(a[i]%2==0)
+//         printf("%d ",a[i]);
+
+//     }printf("\n");
+//     printf("odd");
+//     for(i=0;i<n;i++){
+//         if(a[i]%2!=0)
+//             printf("%d",a[i]);
+//     }
+// }
